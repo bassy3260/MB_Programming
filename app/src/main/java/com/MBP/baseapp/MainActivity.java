9a08a61c89e0f006917d135a4b44ed8e2a2113e7
bassy3260 <bassy3260@naver.com>
2022-09-08 14:31:38 +0900

    week1: settings, button

--- app/src/main/java/com/MBP/baseapp/MainActivity.java
+++ app/src/main/java/com/MBP/baseapp/MainActivity.java
@@ -3,12 +3,25 @@ package com.MBP.baseapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button1= (Button) findViewById(R.id.strBtn1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.v("test","버튼1");
+            }
+        });
     }
+
+
+
 }
